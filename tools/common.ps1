c858ed3b2b228e9c77ea820a7d7538a91b1c7d90
Owen Smith <owen.smith@citrix.com>
2018-08-13 11:40:37 +0100

    Update URL to WDK10.1.17134.1
    Updates the download URL and checksum to WDK 10.1.17134.1
    
    Signed-off-by: Owen Smith <owen.smith@citrix.com>

--- tools/common.ps1
+++ tools/common.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'WindowsDriverKit10'
 $installerType = 'EXE'
-$url = 'http://download.microsoft.com/download/C/E/1/CE19C726-6036-4443-845B-A652B0F48CD7/wdk/wdksetup.exe'
+$url = 'http://download.microsoft.com/download/B/5/8/B58D625D-17D6-47A8-B3D3-668670B6D1EB/wdk/wdksetup.exe'
 $silentArgs = '/features + /q /norestart'
 $validExitCodes = @(0)
-$checksum = 'c560f3f30f5de483f9a6b7dd30727cc375e8f0f7'
+$checksum = '5BC33F95FE980BA44256329007C25BFF7397EF27'
 $checksumType = 'sha1'
