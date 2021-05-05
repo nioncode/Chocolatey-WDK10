6673a8b0d5f3ab05ee761b4ddb8fa97bf9d005ff
Nicolas Schneider <nioncode+git@gmail.com>
2021-05-05 20:30:15 +0200

    remove installation of Visual Studio Extension
    If Visual Studio is not installed (e.g. because only the VS Build Tools
    are installed), then installing the extension fails.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,4 +1,3 @@
 ﻿. (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'common.ps1')
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -checksum $checksum -checksumType $checksumType -validExitCodes $validExitCodes
-Install-VisualStudioVsixExtension -PackageName "WDK" -VsixUrl "${env:ProgramFiles(x86)}\Windows Kits\10\Vsix\VS2019\WDK.vsix"
